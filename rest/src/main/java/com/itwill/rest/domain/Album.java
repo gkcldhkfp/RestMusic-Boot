147b6a9c1d46aadd615c7791e892a2f8fc339bcd
Youngwung <chenld0728@gmail.com>
2024-08-09 14:19:47 +0900

    album<- songs 연결 성공

---
+++ rest/src/main/java/com/itwill/rest/domain/Album.java
@@ -0,0 +1,43 @@
+package com.itwill.rest.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "ALBUMS")
+@Getter @NoArgsConstructor 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode @Builder @ToString
+public class Album {
+	@Id // PK
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 자동번호생성
+	private Integer albumId;
+
+	private String albumName;
+
+	private String albumImage;
+
+	@Basic(optional = false) // not null
+	private String albumType;
+
+	private LocalDate albumReleaseDate;
+
+	@OneToMany(mappedBy = "album")
+	private List<Song> songs;
+
+}
---
+++ rest/src/test/java/com/itwill/rest/repository/AlbumRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.itwill.rest.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itwill.rest.domain.Album;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+public class AlbumRepositoryTest {
+	
+	@Autowired
+	private AlbumRepository albumRepo;
+	
+	// @Test
+	public void diTest() {
+		log.info("diTest(albumRepo = {})", albumRepo);
+		assertThat(albumRepo).isNotNull();
+	}
+
+	// @Test
+	@Transactional
+	public void findAllTest() {
+		log.info("findAllTest()");
+
+		List<Album> albums = albumRepo.findAll();
+
+		albums.forEach((a) -> {System.out.println(a);});
+		albums.forEach((a) -> {System.out.println(a.getSongs());});
+	}
+
+	@Test
+	@Transactional
+	public void findByIdTest() {
+		log.info("findByIdTest()");
+
+		Album album = albumRepo.findById(3).get(); // 천천히 가 앨범		
+		
+		System.out.println(album);
+		System.out.println(album.getSongs());
+	}
+	
+}
