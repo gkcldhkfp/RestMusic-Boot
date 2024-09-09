6a4496fe19d1980487223f1f1c1045dee7debe7f
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-09-09 15:47:08 +0900

    각 차트 페이지에서 맨 처음에 플로팅 버튼에서 듣기, 재생목록 클릭 시 선택된 개수만큼 추가 안 되는 버그 해결. 초성 검색 repository 쿼리문 수정

--- rest/src/main/java/com/itwill/rest/repository/AlbumRepository.java
+++ rest/src/main/java/com/itwill/rest/repository/AlbumRepository.java
@@ -319,59 +319,59 @@ public interface AlbumRepository extends JpaRepository<Album, Integer> {
 	List<Object[]> searchAlbumsAlphabet(@Param("keyword") String keyword, @Param("offset") int offset);
 	
 
+	@Query(value = "SELECT 'album' AS type, " +
+            "a.album_id AS id, " +
+            "a.album_name AS name, " +
+            "a.album_release_date AS release_date, " +
+            "COUNT(al.album_id) AS like_count " +
+            "FROM albums a " +
+            "JOIN albums_first_con afc ON a.album_id = afc.album_id " +
+            "LEFT JOIN album_likes al ON a.album_id = al.album_id " +
+            "WHERE afc.album_first_con_name LIKE CONCAT('%', :searchTerm, '%') " +
+            "GROUP BY a.album_id, a.album_name, a.album_release_date " +
 
-		@Query(value = "SELECT 'album' AS type, " +
-			"a.album_id AS id, " +
-			"a.album_name AS name, " +
-			"a.album_release_date AS release_date, " +
-			"COUNT(al.album_id) AS like_count " +
-			"FROM albums a " +
-			"JOIN albums_first_con afc ON a.album_id = afc.album_id " +
-			"LEFT JOIN album_likes al ON a.album_id = al.album_id " +
-			"WHERE afc.album_first_con_name LIKE CONCAT('%', :searchTerm, '%') " +
-			"GROUP BY a.album_id, a.album_name, a.album_release_date " +
+            "UNION ALL " +
 
-			"UNION ALL " +
+            "SELECT 'song' AS type, " +
+            "s.song_id AS id, " +
+            "s.title AS name, " +
+            "NULL AS release_date, " +
+            "COUNT(sl.song_id) AS like_count " +
+            "FROM songs s " +
+            "JOIN songs_first_con sfc ON s.song_id = sfc.song_id " +
+            "LEFT JOIN likes sl ON s.song_id = sl.song_id " +
+            "WHERE sfc.song_first_con_name LIKE CONCAT('%', :searchTerm, '%') " +
+            "GROUP BY s.song_id, s.title " +
 
-			"SELECT 'song' AS type, " +
-			"s.song_id AS id, " +
-			"s.title AS name, " +
-			"NULL AS release_date, " +
-			"COUNT(sl.song_id) AS like_count " +
-			"FROM songs s " +
-			"JOIN songs_first_con sfc ON s.song_id = sfc.song_id " +
-			"LEFT JOIN likes sl ON s.song_id = sl.song_id " +
-			"WHERE sfc.song_first_con_name LIKE CONCAT('%', :searchTerm, '%') " +
-			"GROUP BY s.song_id, s.title " +
+            "UNION ALL " +
 
-			"UNION ALL " +
+            "SELECT 'group' AS type, " +
+            "g.group_id AS id, " +
+            "g.group_name AS name, " +
+            "NULL AS release_date, " +
+            "COUNT(gl.group_id) AS like_count " +
+            "FROM `groups` g " +
+            "JOIN groups_first_con gfc ON g.group_id = gfc.group_id " +
+            "LEFT JOIN group_likes gl ON g.group_id = gl.group_id " +
+            "WHERE gfc.group_first_con_name LIKE CONCAT('%', :searchTerm, '%') " +
+            "GROUP BY g.group_id, g.group_name " +
 
-			"SELECT 'group' AS type, " +
-			"g.group_id AS id, " +
-			"g.group_name AS name, " +
-			"NULL AS release_date, " +
-			"COUNT(gl.group_id) AS like_count " +
-			"FROM `groups` g " +
-			"JOIN groups_first_con gfc ON g.group_id = gfc.group_id " +
-			"LEFT JOIN group_likes gl ON g.group_id = gl.group_id " +
-			"WHERE gfc.group_first_con_name LIKE CONCAT('%', :searchTerm, '%') " +
-			"GROUP BY g.group_id, g.group_name " +
+            "UNION ALL " +
 
-			"UNION ALL " +
+            "SELECT 'artist' AS type, " +
+            "art.artist_id AS id, " +
+            "art.artist_name AS name, " +
+            "NULL AS release_date, " +
+            "COUNT(artl.artist_id) AS like_count " +
+            "FROM artists art " +
+            "JOIN artists_first_con afc ON art.artist_id = afc.artist_id " +
+            "LEFT JOIN artist_likes artl ON art.artist_id = artl.artist_id " +
+            "WHERE afc.artist_first_con_name LIKE CONCAT('%', :searchTerm, '%') " +
+            "GROUP BY art.artist_id, art.artist_name " +
 
-			"SELECT 'artist' AS type, " +
-			"art.artist_id AS id, " +
-			"art.artist_name AS name, " +
-			"NULL AS release_date, " +
-			"COUNT(artl.artist_id) AS like_count " +
-			"FROM artists art " +
-			"JOIN artists_first_con afc ON art.artist_id = afc.artist_id " +
-			"LEFT JOIN artist_likes artl ON art.artist_id = artl.artist_id " +
-			"WHERE afc.artist_first_con_name LIKE CONCAT('%', :searchTerm, '%') " +
-			"GROUP BY art.artist_id, art.artist_name " +
-
-			"ORDER BY LENGTH(name), like_count DESC",
-			nativeQuery = true)
+            "ORDER BY LENGTH(name), like_count DESC " +
+            "LIMIT 5",
+            nativeQuery = true)
 	List<Object[]> findChoSeongBySearchTerm(@Param("searchTerm") String searchTerm);
 }
 
